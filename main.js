f3a27b5bdc2174b2387e3da78ba4f1a43872963b
myhed <capuchondu92@hotmail.fr>
2018-05-09 09:47:24 +0000

    add comment exemple for exercice

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 // Objectif savoir trier les cartes comme tu le souhaite.
 // Tu auras besoin de la fonction filter et map.
+
+//exemple je veux juste que les dame globalement sois afficher 
+//donc dame , dame de carreau , de pique , de coeur dois être afficher et seulement elle
