33eba03c5feced1a74091bb29deaccc21d145e48
myhed <capuchondu92@hotmail.fr>
2018-05-09 15:32:52 +0000

    add some logique javascript

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 // Objectif savoir trier les cartes comme tu le souhaite.
 // Tu auras besoin de la fonction filter et map.
 
-//exemple je veux juste que les dames globalement sois afficher 
-//donc dame , dame de carreau , de pique , de coeur doit être afficher et seulement elles
+window.onload = function(){
+    
+    var plateform = document.getElementsByClassName('plateform');
+    var want = "carreau"
+    //console.log(Array.from(cartes));
+    const plateformChild = Array.from(plateform[0].children);
+    
+    classCards = plateformChild.map(function(card,index){
+         return card.className.split(' ');
+    });
+    
+    const res = classCards.filter((ArrayOfClass,index) => {
+        const position = ArrayOfClass.indexOf(want)
+        let response;
+        
+        if(position !== -1){
+            switch(want){
+                case "dame":  
+                    response = ArrayOfClass.join(' ');
+                 break;
+                case"roi": 
+                   response = ArrayOfClass.join(' ') 
+                break;
+                default:
+                    response = ArrayOfClass[position]
+                break;
+            }
+           return ArrayOfClass.join(' ');
+        }
+        
+    });
+    
+        console.log(res);
+    
+};
